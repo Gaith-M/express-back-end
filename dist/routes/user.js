05f140a5152074fec701fd03a9f52e4933cef24a
Gaith <mhdgaith94@gmail.com>
2022-06-17 14:52:41 +0300

    /user/:id route.. fetches the user with given id

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const fs_1 = __importDefault(require("fs"));
 const router = (0, express_1.Router)();
 router.post('/', (req, res) => {
     console.log(req.body);
@@ -8,7 +12,17 @@ router.post('/', (req, res) => {
 });
 router.route('/:id')
     .get((req, res) => {
-    res.status(200).send('get');
+    fs_1.default.readFile(`${process.cwd()}/mock-db/users.json`, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Something went wrong');
+        }
+        let parsedData = JSON.parse(data.toString());
+        let user = parsedData.users.filter(user => user.id === Number(req.params.id));
+        if (user.length > 0)
+            return res.status(200).send(user[0]);
+        res.status(404).send(`User with id of ${req.params.id} wasn't found`);
+    });
 })
     .put((req, res) => {
     res.status(200).send('put');
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,6 @@
-import {Request, Response, Router} from 'express';
+import {query, Request, Response, Router} from 'express';
+import fs from 'fs'
+import { User } from '../interfaces';
 
 const router = Router();
 
@@ -10,7 +12,21 @@ router.post('/', (req: Request, res: Response) => {
 
 router.route('/:id')
     .get((req: Request, res: Response) => {
-        res.status(200).send('get');
+
+        fs.readFile(`${process.cwd()}/mock-db/users.json`, (err, data) => {
+            if(err){
+                console.log(err);
+                res.status(500).send('Something went wrong');
+            }
+
+            let parsedData: {users: User[]} = JSON.parse(data.toString());
+            
+            let user = parsedData.users.filter(user => user.id === Number(req.params.id));
+
+            if(user.length > 0) return res.status(200).send(user[0]);
+
+            res.status(404).send(`User with id of ${req.params.id} wasn't found`)
+        })
     })
     .put((req: Request, res: Response) => {
         res.status(200).send('put');
